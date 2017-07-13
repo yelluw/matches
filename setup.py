29c7dde638ea0f9e5d49ce38d6ae3fac6cd875c2
yelluw <pr@yelluw.com>
2017-07-12 23:07:20 -0400

    Updated setup.py with release URL

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Pablo Rivera",
     author_email="pr@yelluw.com",
     url="https://github.com/yelluw/matches",
-    download_url="https://github.com/yelluw/matches/archive/0.1.0.tar.gz",
+    download_url="https://github.com/yelluw/matches/archive/0.1.1.tar.gz",
     keywords=["pattern matching", "Python 3", "Yelluw"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
